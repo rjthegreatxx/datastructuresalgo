b11ed1ad40a4c978f827eeeb284d74886112e51f
RJ Militante <rj.militante@payzer.com>
2021-10-13 21:10:06 -0500

    ore problems from lc

---
+++ src/com/kraftwerking/datastructuresalgo/datastructurescourse/leetcode/ContainerWithMostWater11.java
@@ -0,0 +1,29 @@
+package com.kraftwerking.datastructuresalgo.datastructurescourse.leetcode;
+
+
+public class ContainerWithMostWater11 {
+  public int maxArea(int[] height) {
+    int res = 0;
+    int l = 0;
+    int r = height.length - 1;
+    while(l < r){
+      int area = (r -l) * Math.min(height[l], height[r]);
+      res = Math.max(res, area);
+      if(height[l] < height[r]) {
+        l++;
+      } else {
+        r--;
+      }
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) {
+    int[] height = {1,8,6,2,5,4,8,3,7};
+
+    ContainerWithMostWater11 containerWithMostWater11 = new ContainerWithMostWater11();
+    int result = containerWithMostWater11.maxArea(height);
+    System.out.println(result);
+  }
+}
---
+++ src/com/kraftwerking/datastructuresalgo/datastructurescourse/leetcode/MinimumInRotatedSortedArray153.java
@@ -0,0 +1,27 @@
+package com.kraftwerking.datastructuresalgo.datastructurescourse.leetcode;
+
+
+public class MinimumInRotatedSortedArray153 {
+
+  public int findMin(int[] nums) {
+    int low = 0;
+    int high = nums.length -1;
+    while(low < high){
+      int mid = (low + high)/2;
+      if(nums[mid] < nums[high]){
+        high = mid;
+      } else {
+        low = mid + 1;
+      }
+    }
+
+    return nums[low];
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {3,4,5,1,2};
+    MinimumInRotatedSortedArray153 minimumInRotatedSortedArray153 = new MinimumInRotatedSortedArray153();
+    int result = minimumInRotatedSortedArray153.findMin(nums);
+    System.out.println(result);
+  }
+}
