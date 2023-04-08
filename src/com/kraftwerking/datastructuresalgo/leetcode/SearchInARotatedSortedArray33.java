c731f49008141fd2d90a163911e2be3f5f8b68df
RJ Militante <redmond007@gmail.com>
2023-04-07 21:05:23 -0500

    more problems, nc75

--- src/com/kraftwerking/datastructuresalgo/leetcode/SearchInARotatedSortedArray33.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/SearchInARotatedSortedArray33.java
@@ -4,14 +4,15 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 public class SearchInARotatedSortedArray33 {
   public int search(int[] nums, int target) {
 
-    return -1;
+    return 1;
   }
 
   public static void main(String[] args) {
-    int[] nums  = {11,13,15,17};
+    int[] nums = {4,5,6,7,0,1,2};
+
     SearchInARotatedSortedArray33 searchInARotatedSortedArray33 = new SearchInARotatedSortedArray33();
     int result = searchInARotatedSortedArray33.search(nums, 0);
-    System.out.println(result);
+
 
   }
 }
