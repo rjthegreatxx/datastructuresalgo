ca29a76bb34669ae740c87fcff226a6c9ff927a4
RJ Militante <redmond007@gmail.com>
2023-04-16 12:58:11 -0500

    more problems, nc75

---
+++ src/com/kraftwerking/datastructuresalgo/leetcode/InsertionSort.java
@@ -0,0 +1,17 @@
+package com.kraftwerking.datastructuresalgo.leetcode;
+
+
+public class InsertionSort {
+  public static int[] insertionSort(int[] arr) {
+    for (int i = 1; i < arr.length; i++) {
+      int j = i - 1;
+      while (j >= 0 && arr[j+1] < arr[j]) {
+        int tmp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = tmp;
+        j--;
+      }
+    }
+    return arr;
+  }
+}
