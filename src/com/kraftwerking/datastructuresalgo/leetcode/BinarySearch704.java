996243c396bf244619a8ab7ead7cc75c4fbbf52e
RJ Militante <redmond007@gmail.com>
2023-04-17 23:00:40 -0500

    more problems, nc75

--- src/com/kraftwerking/datastructuresalgo/leetcode/BinarySearch704.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/BinarySearch704.java
@@ -2,26 +2,29 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 
 public class BinarySearch704 {
   public int search(int[] nums, int target) {
-    int L = 0, R = nums.length - 1;
-    int mid;
+    int l = 0;
+    int r = nums.length - 1;
 
-    while(L<=R){
-      mid = (L + R)/2;
+    while(l <= r){
+      int mid = (r + l) / 2;
+
       if(target > nums[mid]){
-        L = mid + 1;
-      } else if (target < nums[mid]){
-        R = mid -1;
+        l = mid + 1;
+      }else if(target < nums[mid]){
+        r = mid - 1;
       } else {
         return mid;
       }
     }
+
     return -1;
   }
 
   public static void main(String[] args){
     int[] nums = {-1,0,3,5,9,12};
     BinarySearch704 binarySearch704 = new BinarySearch704();
-    binarySearch704.search(nums, 9);
+    int res = binarySearch704.search(nums, 9);
+    System.out.println(res);
   }
 
 }
--- src/com/kraftwerking/datastructuresalgo/leetcode/SearchA2DMatrix74.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/SearchA2DMatrix74.java
@@ -2,68 +2,33 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 
 public class SearchA2DMatrix74 {
   public boolean searchMatrix(int[][] matrix, int target) {
-    int rows = matrix.length;
     int cols = matrix[0].length;
     int top = 0;
     int bottom = matrix.length - 1;
     int row = -1;
+    int l = 0;
+    int r = matrix[0].length - 1;
 
-    while(top <= bottom){
-      if(top == bottom) {
-        row = top;
-        break;
-      } else if(target > matrix[top][cols-1]){
+    while(top<=bottom){
+      if(target > matrix[top][cols - 1]){
         top++;
       } else if(target < matrix[bottom][0]){
         bottom--;
+      } else if(top == bottom){
+        row = top;
+        break;
       }
     }
 
-    if(row == -1) {
-      return false;
-    }
+    if(row == -1)return false;
 
-    int L = 0, R = matrix[row].length - 1;
-    int mid;
+    while(l<=r){
+      int mid = (l + r)/2;
 
-    while (L <= R) {
-      mid = (L + R) / 2;
-      if (target > matrix[row][mid]) {
-        L = mid + 1;
-      } else if (target < matrix[row][mid]) {
-        R = mid - 1;
-      } else {
-        return true;
-      }
-    }
-
-    return false;
-  }
-
-  public boolean searchMatrixAttempt1(int[][] matrix, int target) {
-    int a = matrix[0].length;
-    int b = matrix.length;
-    int newSz = a * b;
-    int rowCount = 0;
-
-    int[] flat = new int[newSz];
-    for(int i = 0;i<matrix.length;i++){
-      int[] row = matrix[i];
-      for(int j = 0;j<row.length;j++){
-        flat[rowCount] = row[j];
-        rowCount++;
-      }
-    }
-
-    int L = 0, R = flat.length - 1;
-    int mid;
-
-    while (L <= R) {
-      mid = (L + R) / 2;
-      if (target > flat[mid]) {
-        L = mid + 1;
-      } else if (target < flat[mid]) {
-        R = mid - 1;
+      if(target > matrix[row][mid]){
+        l = mid + 1;
+      } else if(target < matrix[row][mid]){
+        r = mid - 1;
       } else {
         return true;
       }
@@ -76,7 +41,8 @@ public class SearchA2DMatrix74 {
     int[][] matrix  = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
     SearchA2DMatrix74 searchA2DMatrix74 = new SearchA2DMatrix74();
     boolean result = searchA2DMatrix74.searchMatrix(matrix, 3);
-    int i = 0;
+    System.out.println(result);
+
   }
 
 }
