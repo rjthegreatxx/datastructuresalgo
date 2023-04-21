b3246d9a8f1a5af83be3f65a7a1a766d54d9c506
RJ Militante <redmond007@gmail.com>
2023-04-20 20:57:28 -0500

    more problems, nc75

--- src/com/kraftwerking/datastructuresalgo/leetcode/ValidParentheses20.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/ValidParentheses20.java
@@ -1,35 +1,29 @@
 package com.kraftwerking.datastructuresalgo.leetcode;
 
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Stack;
 
 public class ValidParentheses20 {
   public boolean isValid(String s) {
-    if(s.isEmpty()) return false;
-    List<Character> stack = new ArrayList<>();
-    Map<String, String> closeToOpen = new HashMap<>();
-    closeToOpen.put("}","{");
-    closeToOpen.put("]","[");
-    closeToOpen.put(")","(");
-
-    char[] chars = s.toCharArray();
-    if(chars.length == 1) return false;
-    for(int i = 0;i< chars.length;i++){
-      char c = chars[i];
-      if(closeToOpen.containsKey(String.valueOf(c))){
-        String open = closeToOpen.get(String.valueOf(c));
-        String fromStack = String.valueOf(stack.remove(stack.size() - 1));
-        if(!open.equals(fromStack)) return false;
-      } else {
-        if(!closeToOpen.keySet().contains(c)) return false;
-        stack.add(c);
+    if (s.length() % 2 != 0) return false;
+    Stack<Character> stack = new Stack<>();
+    for (int i = 0; i < s.length(); i++) {
+      if (
+              stack.isEmpty() &&
+                      (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']')
+      ) return false; else {
+        if (!stack.isEmpty()) {
+          if (
+                  stack.peek() == '(' && s.charAt(i) == ')'
+          ) stack.pop(); else if (
+                  stack.peek() == '{' && s.charAt(i) == '}'
+          ) stack.pop(); else if (
+                  stack.peek() == '[' && s.charAt(i) == ']'
+          ) stack.pop(); else stack.add(s.charAt(i));
+        } else stack.add(s.charAt(i));
       }
     }
-
-    return true;
+    return stack.isEmpty();
   }
 
   public static void main(String[] args) {
