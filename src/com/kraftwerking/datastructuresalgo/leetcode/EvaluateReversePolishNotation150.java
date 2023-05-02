c6c0308a692526f5f269873f1fab6faca9e4ea73
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2023-05-02 11:52:51 -0500

    evaluate reverse polish

---
+++ src/com/kraftwerking/datastructuresalgo/leetcode/EvaluateReversePolishNotation150.java
@@ -0,0 +1,37 @@
+package com.kraftwerking.datastructuresalgo.leetcode;
+
+
+public class FirstBadVersion278 {
+  public int firstBadVersion(int n) {
+    int mid;
+    int low = 1;
+    int high = n;
+
+    while(low<=high){
+      mid = low + (high-low)/2;
+
+      if(isBadVersion(mid) == false){
+        low = mid + 1;
+
+      }else if(isBadVersion(mid) == true){
+        high = mid - 1;
+
+      }
+    }
+    return low;
+
+  }
+
+  private boolean isBadVersion(int n) {
+    if(n >= 4) return true;
+    return false;
+  }
+
+  public static void main(String[] args) {
+    String s = "226";
+    FirstBadVersion278 firstBadVersion278 = new FirstBadVersion278();
+    int result = firstBadVersion278.firstBadVersion(5);
+    System.out.println(result);
+
+  }
+}
