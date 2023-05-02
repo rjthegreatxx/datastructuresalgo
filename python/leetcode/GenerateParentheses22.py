d175049a2ce50b90a60669cae4a2c3014fc022c3
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2023-05-02 16:47:31 -0500

    more problems, nc75

---
+++ python/leetcode/GenerateParentheses22.py
@@ -0,0 +1,29 @@
+from typing import List
+
+# only add open paren if open < n
+# only add close paren if closed < open
+# valid IIF open == closed == n
+
+
+class Solution(object):
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
