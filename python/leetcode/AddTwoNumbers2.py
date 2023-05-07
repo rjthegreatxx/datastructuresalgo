c4fc998f61c9bd4cd73807f13728dd2c5a9ce1aa
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2023-05-07 17:41:38 -0500

    more problems, nc75

--- python/leetcode/AddTwoNumbers2.py
+++ python/leetcode/AddTwoNumbers2.py
@@ -1,27 +1,40 @@
-from typing import List
+from typing import List, Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        s = ""
-        cur = l1
-        while(cur):
-            s = s + str(cur.val)
-            cur = cur.next
-        str1 = s[::-1]
+        dummy = ListNode()
+        cur = dummy
 
-        s = ""
-        cur = l2
-        while(cur):
-            s = s + str(cur.val)
-            cur = cur.next
-        str2 = s[::-1]
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-        return 2
+            # new digit
+            val = v1 + v2 + carry
+            carry = val // 10
+            val = val % 10
+            cur.next = ListNode(val)
+
+            # update ptrs
+            cur = cur.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+
+        # 8 + 7
+        return dummy.next
+
 
 def main():
-    l1 = [2,4,3]
-    l2 = [5,6,4]
+    l1 = [2, 4, 3]
+    l2 = [5, 6, 4]
 
     solution = Solution()
     result = solution.addTwoNumbers(l1, l2)
