c769717b54623ef55c759c0f75dc65eabe44fa1c
RJ Militante <redmond007@gmail.com>
2023-11-22 07:50:05 -0600

    Latest

---
+++ src/com/kraftwerking/datastructuresalgo/leetcode/ReverseLinkedList2062.java
@@ -0,0 +1,22 @@
+package com.kraftwerking.datastructuresalgo.leetcode;
+
+class ReverseLinkedList2062 {
+
+  public ListNode reverseList(ListNode head) {
+    ListNode curr = head;
+    ListNode prev = null;
+
+    while(curr != null){
+      ListNode tmp = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = tmp;
+    }
+
+    return prev;
+  }
+  public static void main(String[] args) {
+    ReverseLinkedList2062 reverseLinkedList206 = new ReverseLinkedList2062();
+  }
+}
+
