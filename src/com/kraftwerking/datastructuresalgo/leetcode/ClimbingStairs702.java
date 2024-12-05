7bc5234cc583dece96c39d286eaa2944a2700b61
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2024-12-05 09:52:54 -0600

    Update ClimbingStairs702.java
    latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/ClimbingStairs702.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/ClimbingStairs702.java
@@ -3,8 +3,13 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 class ClimbingStairs702 {
 
   public int climbStairs(int n) {
-    return 1;
-  }
+    return dfs(n, 0); 
+}
+
+public int dfs(int n, int i) {
+    if (i >= n) return i == n ? 1 : 0;
+    return dfs(n, i + 1) + dfs(n, i + 2);
+}
 
   public static void main(String[] args) {
     ClimbingStairs702 climbingStairs70 = new ClimbingStairs702();
