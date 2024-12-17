bf44b4e139ca7c2fee144dc50691fcd8c159a2ba
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2024-12-17 11:35:20 -0600

    Update ValidAnagram242.java
    latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/ValidAnagram242.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/ValidAnagram242.java
@@ -1,36 +1,22 @@
 package com.kraftwerking.datastructuresalgo.leetcode;
 
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ValidAnagram242 {
-  public boolean isAnagram(String s, String t) {
-    Map<Character, Integer> characterMap = new HashMap<>();
-    char[] arr1 = s.toCharArray();
-    char[] arr2 = t.toCharArray();
-
-    for(char c:arr1){
-      characterMap.put(c, characterMap.getOrDefault(c, 0) + 1);
-    }
-
-    for(char c:arr2){
-      if(!characterMap.containsKey(c)){
+      public boolean isAnagram(String s, String t) {
+        char[] charArray = s.toCharArray();
+        Arrays.sort(charArray);
+        String ss = new String(charArray);
+        charArray = t.toCharArray();
+        Arrays.sort(charArray);
+        String st = new String(charArray);
+        if(ss.equals(st)) return true;
         return false;
-      } else {
-        characterMap.put(c, characterMap.getOrDefault(c, 0) + 1);
-      }
+        
     }
-
-    int total = 0;
-    for(int val:characterMap.values()){
-      total = total + val;
-    }
-
-    if(total > 0) return false;
-
-    return true;
-  }
 
   public static void main(String[] args) {
     ValidAnagram242 validAnagram242 = new ValidAnagram242();
