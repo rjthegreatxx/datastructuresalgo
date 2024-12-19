f2b337e47a478da30129044178a4a6a908b81376
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2024-12-19 13:53:40 -0600

    latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/GroupAnagrams492.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/GroupAnagrams492.java
@@ -1,20 +1,23 @@
 package com.kraftwerking.datastructuresalgo.leetcode;
 
+
 import java.util.*;
 
-class GroupAnagrams49 {
+class GroupAnagrams492 {
 
   public List<List<String>> groupAnagrams(String[] strs) {
     List<List<String>> res = new ArrayList<>();
     Map<String, List<String>> anMap = new HashMap<>();
 
-    for (String s : strs) {
+    for(String s: strs){
       char[] arr = s.toCharArray();
       Arrays.sort(arr);
       String sortedStr = new String(arr);
 
-      if (anMap.containsKey(sortedStr)) {
-        anMap.get(sortedStr).add(s);
+      if(anMap.containsKey(sortedStr)){
+        List<String> newList = anMap.get(sortedStr);
+        newList.add(s);
+        anMap.put(sortedStr,newList);
 
       } else {
         List<String> newList = new ArrayList<>();
@@ -23,7 +26,7 @@ class GroupAnagrams49 {
       }
     }
 
-    for (Map.Entry<String, List<String>> entry : anMap.entrySet()) {
+    for(Map.Entry<String, List<String >> entry: anMap.entrySet()) {
       res.add(entry.getValue());
     }
 
@@ -31,9 +34,9 @@ class GroupAnagrams49 {
   }
 
   public static void main(String[] args) {
-    String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
-    GroupAnagrams49 groupAnagrams49 = new GroupAnagrams49();
-    List<List<String>> result = groupAnagrams49.groupAnagrams(strs);
+    String[] strs = {"eat","tea","tan","ate","nat","bat"};
+    GroupAnagrams492 groupAnagrams492 = new GroupAnagrams492();
+    List<List<String>> result = groupAnagrams492.groupAnagrams(strs);
     System.out.println(result);
   }
 }
