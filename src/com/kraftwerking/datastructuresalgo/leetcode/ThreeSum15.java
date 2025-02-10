0775a7be980d06bb590db7bb88ebe039a096e3f9
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-02-10 11:38:20 -0600

    latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/ThreeSum15.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/ThreeSum15.java
@@ -2,47 +2,43 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 class ThreeSum15 {
 
-  //2 pointers
-  public List<List<Integer>> threeSum(int[] nums) {
-    Arrays.sort(nums);
-    LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
-
-    for (int i = 0; i < nums.length - 2; i++) {
-      if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-        int target = 0 - nums[i];
-        int left = i + 1;
-        int right = nums.length - 1;
-
-        while (left < right) {
-          if (nums[left] + nums[right] == target) {
-            ArrayList<Integer> miniSol = new ArrayList<>();
-            miniSol.add(nums[i]);
-            miniSol.add(nums[left]);
-            miniSol.add(nums[right]);
-            sol.add(miniSol);
-            while (left < right && nums[left] == nums[left + 1]) {
-              left++;
-            }
-            while (left < right && nums[right] == nums[right - 1]) {
-              right--;
-            }
-            left++;
-            right--;
-          } else if (nums[left] + nums[right] > target) {
-            right--;
-          } else {
-            left++;
-          }
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-      }
-    }
 
-    return sol;
-  }
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            count.put(nums[i], count.get(nums[i]) - 1);
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.get(nums[j]) - 1);
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int target = -(nums[i] + nums[j]);
+                if (count.getOrDefault(target, 0) > 0) {
+                    res.add(Arrays.asList(nums[i], nums[j], target));
+                }
+            }
+
+            for (int j = i + 1; j < nums.length; j++) {
+                count.put(nums[j], count.get(nums[j]) + 1);
+            }
+        }
+        return res;
+    }
 
   public static void main(String[] args) {
     int[] nums = {-1, 0, 1, 2, -1, -4};
