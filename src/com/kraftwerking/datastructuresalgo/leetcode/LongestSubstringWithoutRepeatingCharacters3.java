a286d6a72daec4e021654d06574971b62b338784
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-02-26 14:00:04 -0600

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/LongestSubstringWithoutRepeatingCharacters3.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/LongestSubstringWithoutRepeatingCharacters3.java
@@ -7,25 +7,27 @@ import java.util.Set;
 class LongestSubstringWithoutRepeatingCharacters3 {
 
   public int lengthOfLongestSubstring(String s) {
-    if (s.length() <= 1) {return s.length();}
+    if (s.length() <= 1) {
+      return s.length();
+    }
     int res = 0;
 
-    for (int l = 0; l < s.length() - 1; l++) {
-      Set<String > charSet = new HashSet<>();
-      charSet.add(String.valueOf(s.charAt(l)));
-
-      int r = l + 1;
+    for (int i = 0; i < s.length(); i++) {
+      Set<Character> charSet = new HashSet<Character>();
+      charSet.add(s.charAt(i));
       int len = 1;
-      while(r<s.length()){
-        if(!charSet.contains(String.valueOf(s.charAt(r)))){
-          charSet.add(String.valueOf(s.charAt(r)));
-          len++;
-          r++;
-        } else {
+
+      for (int r = i + 1; r < s.length(); r++) {
+        if (charSet.contains(s.charAt(r))) {
           break;
+        } else {
+          charSet.add(s.charAt(r));
+          len++;
         }
       }
+
       res = Math.max(res, len);
+
     }
 
     return res;
