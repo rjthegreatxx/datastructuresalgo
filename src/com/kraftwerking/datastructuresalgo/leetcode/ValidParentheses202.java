e6b579296b25cc06adbbf620874b52eb7b75635b
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-02-27 15:08:14 -0600

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/ValidParentheses202.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/ValidParentheses202.java
@@ -7,12 +7,15 @@ import java.util.HashMap;
 public class ValidParentheses202 {
     public boolean isValid(String s) {
         if(s.isEmpty()) return false;
+
         char[] chars = s.toCharArray();
         HashMap<Character, Character> closeToOpen = new HashMap<>();
         closeToOpen.put(']', '[');
         closeToOpen.put(')', '(');
         closeToOpen.put('}', '{');
+
         ArrayList<Character> stack = new ArrayList<>();
+
         for(int i = 0;i < chars.length; i++){
             if(!stack.isEmpty() && closeToOpen.containsKey(chars[i])){
                 if(closeToOpen.get(chars[i]).equals(stack.get(i-1))){
