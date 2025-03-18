1447e96518c4dc33b17435f697dca4577022f7e4
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-03-18 17:04:02 -0500

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/MinStack155.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/MinStack155.java
@@ -2,21 +2,30 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class MinStack155 {
   ArrayList<Integer> stack;
+  ArrayList<Integer> minStack;
 
   public MinStack155() {
     this.stack = new ArrayList<>();
+    this.minStack = new ArrayList<>();
+
   }
 
   public void push(int val) {
     stack.add(val);
+    if (minStack.size() > 0) {
+      int minVal = Math.min(val, minStack.get(minStack.size() - 1));
+      minStack.add(minVal);
+    } else {
+      minStack.add(val);
+    }
   }
 
   public void pop() {
     stack.remove(stack.size() - 1);
+    minStack.remove(minStack.size() - 1);
   }
 
   public int top() {
@@ -24,6 +33,10 @@ public class MinStack155 {
   }
 
   public int getMin() {
-    return Collections.min(stack);
+    if (minStack.size() > 0) {
+      return minStack.get(minStack.size() - 1);
+    } else {
+      return 0;
+    }
   }
 }
