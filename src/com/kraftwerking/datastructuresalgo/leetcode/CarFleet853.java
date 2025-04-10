52e8d87c136e3c71a6234ca89d7a00a7451c7bc2
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-04-10 16:02:01 -0500

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/CarFleet853.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/CarFleet853.java
@@ -1,10 +1,25 @@
 package com.kraftwerking.datastructuresalgo.leetcode;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 class CarFleet853 {
 
   public int carFleet(int target, int[] position, int[] speed) {
-
-    return 0;
-  }
-
+    int[][] pair = new int[position.length][2];
+    for (int i = 0; i < position.length; i++) {
+      pair[i][0] = position[i];
+      pair[i][1] = speed[i];
+    }
+      Arrays.sort(pair, (a, b) -> Integer.compare(b[0], a[0]));
+      Stack<Double> stack = new Stack<>();
+      for (int[] p : pair) {
+        stack.push((double) (target - p[0]) / p[1]);
+        if (stack.size() >= 2 &&
+            stack.peek() <= stack.get(stack.size() - 2)) {
+          stack.pop();
+        }
+      }
+      return stack.size();
+    }
 }
