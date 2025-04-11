da93534bf6257a3f7b88f3fa9824bb38c5689ce3
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-04-11 13:29:54 -0500

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/BinarySearch704.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/BinarySearch704.java
@@ -1,23 +1,18 @@
 package com.kraftwerking.datastructuresalgo.leetcode;
 
 public class BinarySearch704 {
+  public int binary_search(int l, int r, int[] nums, int target) {
+    if (l > r)
+      return -1;
+    int m = l + (r - l) / 2;
+
+    if (nums[m] == target)
+      return m;
+    return (nums[m] < target) ? binary_search(m + 1, r, nums, target) : binary_search(l, m - 1, nums, target);
+  }
+
   public int search(int[] nums, int target) {
-    int l = 0;
-    int r = nums.length - 1;
-
-    while(l <= r){
-      int mid = (r + l) / 2;
-
-      if(target > nums[mid]){
-        l = mid + 1;
-      }else if(target < nums[mid]){
-        r = mid - 1;
-      } else {
-        return mid;
-      }
-    }
-
-    return -1;
+    return binary_search(0, nums.length - 1, nums, target);
   }
 
   public static void main(String[] args){
