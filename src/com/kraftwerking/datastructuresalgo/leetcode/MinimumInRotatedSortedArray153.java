4f10dd5a696582171c9e58badd9dddbf4ac18308
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-05-28 12:55:39 -0500

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/MinimumInRotatedSortedArray153.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/MinimumInRotatedSortedArray153.java
@@ -4,19 +4,26 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 public class MinimumInRotatedSortedArray153 {
 
   public int findMin(int[] nums) {
-    int low = 0;
-    int high = nums.length - 1;
-    while (low < high) {
-      int mid = (low + high) / 2;
-      if (nums[mid] < nums[high]) {
-        high = mid;
-      } else {
-        low = mid + 1;
-      }
-    }
+    int l = 0;
+    int r = nums.length - 1;
+    int res = nums[0];
 
-    return nums[low];
-  }
+    while (l <= r) {
+        if (nums[l] < nums[r]) {
+            res = Math.min(res, nums[l]);
+            break;
+        }
+
+        int m = l + (r - l) / 2;
+        res = Math.min(res, nums[m]);
+        if (nums[m] >= nums[l]) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    return res;
+}
 
   public static void main(String[] args) {
     int[] nums = {3, 4, 5, 1, 2};
