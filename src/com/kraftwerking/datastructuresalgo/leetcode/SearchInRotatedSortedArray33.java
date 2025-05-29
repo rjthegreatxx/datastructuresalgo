048873d11611c8a2fcf24bfe4f8ad525202af046
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-05-29 15:02:30 -0500

    Latest

--- src/com/kraftwerking/datastructuresalgo/leetcode/SearchInRotatedSortedArray33.java
+++ src/com/kraftwerking/datastructuresalgo/leetcode/SearchInRotatedSortedArray33.java
@@ -4,42 +4,31 @@ package com.kraftwerking.datastructuresalgo.leetcode;
 public class SearchInRotatedSortedArray33 {
 
   public int search(int[] nums, int target) {
-    if (nums == null || nums.length == 0) {
-      return -1;
-    }
+    int l = 0;
+    int r = nums.length - 1;
 
-    //binary search to find where array is shifted, pivot point
-    int left = 0;
-    int right = nums.length - 1;
-    while (left < right) {
-      int mid = left + (right - left) / 2;
-      if (nums[mid] > nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid;
-      }
-    }
+    while (l <= r) {
 
-    int realmid = left;
-    left = 0;
-    right = nums.length - 1;
+      int mid = (l + r) / 2;
 
-    //search to r or l side of realmid?
-    if (target >= nums[realmid] && target <= nums[right]) {
-      left = realmid;
-    } else {
-      right = realmid;
-    }
-
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
       if (nums[mid] == target) {
         return mid;
-      } else if (nums[mid] < target) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
       }
+
+        if (nums[l] <= nums[mid]) {
+          if (target > nums[mid] || target < nums[l]) {
+            l = mid + 1;
+          } else {
+              r = mid - 1;
+            }
+          } else {
+          if (target < nums[mid] || target > nums[r]) {
+            r = mid - 1;
+          } else {
+            l = mid + 1;
+          }
+        }
+
     }
 
     return -1;
