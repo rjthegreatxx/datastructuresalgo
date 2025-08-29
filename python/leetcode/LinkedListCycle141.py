c0ffbea4942cac578b4d9d754a38f534677b4820
RJ Militante <53840945+rjthegreatxx@users.noreply.github.com>
2025-08-29 10:51:43 -0500

    Latest

---
+++ python/leetcode/LinkedListCycle141.py
@@ -0,0 +1,15 @@
+from typing import List, Optional
+
+from python.leetcode.LinkedList import ListNode
+
+
+class Solution(object):
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
